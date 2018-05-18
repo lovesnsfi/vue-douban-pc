9ba2e76c3c459f8a2af494e7c248d060c3c0d08e
lovesnsfi <lovesnsfi@163.com>
2018-05-18 11:36:46 +0800

    second commit

---
+++ src/components/m_header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="m_header">
+        
+    </div>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .m_header
+    {
+        
+    }
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="index">
+        <m_header></m_header>
+    </div>
+</template>
+
+<script>
+import m_header from "@/components/m_header.vue";
+export default {
+    name:"index",
+    components:{
+        m_header
+    }
+}
+</script>
+
+<style>
+
+</style>
